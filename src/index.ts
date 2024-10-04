52bf1bca2c85b77371f2baa31c18cb1343e9aeed
carimatics <github.carimatics@gmail.com>
2024-10-04 21:10:58 +0900

    add index.html and setup electron window in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Happy developing ✨')
+const {
+  app, // アプリケーションのイベントライフサイクルを制御する
+  BrowserWindow // ウィンドウを作成、管理するクラス
+} = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
